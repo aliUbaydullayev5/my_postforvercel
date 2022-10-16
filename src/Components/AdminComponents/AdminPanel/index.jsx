c8124cc9047382896a17b487b0bb580fbd1e4456
Ali <65356558+aliUbaydullayev5@users.noreply.github.com>
2022-10-16 12:37:47 +0500

    Add files via upload

--- src/Components/AdminComponents/AdminPanel/index.jsx
+++ src/Components/AdminComponents/AdminPanel/index.jsx
@@ -1,39 +1,39 @@
 import React, {useState} from 'react'
 import {Block} from "./style";
+import axios from "axios";
 
 const AdminPanel = ({changeAdmin}) => {
 
-    const [img, setImg] = useState('')
     const [title, setTitle] = useState('')
     const [desc, setDesc] = useState('')
+    const [file, setFile] = useState(null)
 
-    const pushPost = async () => {
-        if (img.length && title.length && desc){
-            const rawResponse = await fetch('https://preoject2.herokuapp.com/api/addPost', {
+    const pushPost = () => {
+        if (title.length && desc.length) {
+            const formData = new FormData()
+
+            formData.append('img', file)
+            formData.append('title', title)
+            formData.append('desc', desc)
+
+            axios({
+                url: 'https://preoject2.herokuapp.com/api/addPost',
                 method: 'POST',
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                    'Authorization': `Bearer ${localStorage.getItem("token")}`
                 },
-                body: JSON.stringify({
-                    img: img,
-                    title: title,
-                    desc: desc
-                })
-            })
-            const content = await rawResponse.json();
-            if(content?.message && content?.message === 'Success fully saved') {
-                alert('Success fully saved your post')
-                setImg('')
+
+                data: formData
+            }).then((res) => {
+                alert(res.data.message)
                 setTitle('')
                 setDesc('')
-            }
+            })
         }else{
-            alert('you have to top up all "inputs"')
+            alert('do not have Title, Desc')
         }
-
     }
+
 
     const logoutFunc = () => {
         localStorage.removeItem("token")
@@ -43,9 +43,10 @@ const AdminPanel = ({changeAdmin}) => {
     return(
         <Block>
             <h1>Admin Panel</h1>
-            <input type={'text'} placeholder={'Img URL'} onChange={(e)=> setImg(e.target.value)} value={img} />
-            <input type={'text'} placeholder={'Post Title'} onChange={(e)=> setTitle(e.target.value)} value={title} />
-            <input type={'text'} placeholder={'Post Description'} onChange={(e)=> setDesc(e.target.value)} value={desc} />
+
+            <input type={'file'} name={'img'} placeholder={'Img URL'} onChange={(e)=> setFile(e.target.files[0])} />
+            <input type={'text'} name={'title'} placeholder={'Post Title'} onChange={(e)=> setTitle(e.target.value)} value={title} />
+            <input type={'text'} name={'desc'} placeholder={'Post Description'} onChange={(e)=> setDesc(e.target.value)} value={desc} />
             <button onClick={()=> pushPost()} className={'pushButton'}>Push new Property</button>
             <button onClick={()=> logoutFunc()} className={'logButton'}>Logout</button>
         </Block>
@@ -53,3 +54,36 @@ const AdminPanel = ({changeAdmin}) => {
 }
 
 export default AdminPanel
+
+
+//
+//
+// addFile(event) {
+//     var formData = new FormData();
+//     formData.append("file", event.target.files[0]);
+//     formData.append('name', 'some value user types');
+//     formData.append('description', 'some value user types');
+//     console.log(event.target.files[0]);
+//
+//     fetch(`http://.../gallery/${path}`, {
+//         method: 'POST',
+//         headers: {'Content-Type': 'multipart/form-data'},
+//         body: {event.target.files[0]}
+//     })
+//         .then((response) => response.json())
+//         .then((data) => {
+//             this.setState({images: data.images, isLoading: false});
+//             this.props.updateImages(data.images);
+//         })
+//         .catch(error => this.setState({error, isLoading: false}));
+// }
+//
+//
+// render() {
+//     return (
+//         <div>
+//             <form encType="multipart/form-data" action="">
+//                 <input id="id-for-upload-file" onChange={this.addFile.bind(this)} type="file"/>
+//             </form>
+//         </div>)
+// }
--- src/Components/Post/index.jsx
+++ src/Components/Post/index.jsx
@@ -4,7 +4,7 @@ import {Block} from "./style";
 const Post = ({img, title, desc, time, uniqId}) => {
     return(
         <Block id={uniqId}>
-            {img && <Block.Img src={img} />}
+            {img && <Block.Img src={`https://preoject2.herokuapp.com/${img}`} />}
             <Block.Title className={'nocopy'}>
                 <h1>{title}</h1>
                 <p>{time}</p>
