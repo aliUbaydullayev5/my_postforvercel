37d8425fc123b3c22f90984cbdf2ab7a594b83e1
Ali <mukhammadali.ubaydullayev.05@inbox.ru>
2022-11-20 00:05:33 +0500

    change text version to 0.4

--- src/Components/Home/RightBlock/index.jsx
+++ src/Components/Home/RightBlock/index.jsx
@@ -16,7 +16,7 @@ const RightBlock = () => {
                 <h2>Ubaydullayev</h2>
                 <h3>Frontend developer</h3>
                 <h4 onClick={()=> navigate('/about')}>About me</h4>
-                <p> Version 0.3</p>
+                <p> Version 0.4</p>
             </Block.Bottom>
         </Block>
     )
